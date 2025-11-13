9b7ee8b43d3692d89ed43910a9ddad4c56211131
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2025-11-13 13:25:22 -0600

    Updated sign in form

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import forms, login
 
 
 class HomeView(View):
@@ -11,10 +12,30 @@ class HomeView(View):
 class SignInView(View):
 
     def get(self, request):
-        return render(request, "sign-in.html", {})
+        if request.user.is_authenticated:
+            return redirect("home")
+
+        return render(request, "sign-in.html", {
+            "form": forms.AuthenticationForm(request),
+        })
+
+    def post(self, request):
+        form = forms.AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+
+        return render(request, "sign-in.html", {
+            "form": form,
+        })
 
 
 class GetStartedView(View):
 
     def get(self, request):
+        if request.user.is_authenticated:
+            return redirect("home")
+
         return render(request, "get-started.html", {})
