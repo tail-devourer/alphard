33c74292ff4fb8aae41724536d57b52a03f5275b
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2025-12-03 13:59:02 -0600

    Updated templates and removed static links further

--- alphard/urls.py
+++ alphard/urls.py
@@ -11,7 +11,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('blog.urls')),
 ]
 
