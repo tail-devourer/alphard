27fe7b92a1a79237dd37c79d8a375366dc4ef4dc
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2025-12-15 16:40:50 -0600

    On account settings update, it now sends back an email explaining the specific changes

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
 
+
 class User(AbstractUser):
     first_name = None
     last_name = None
--- blog/views/user.py
+++ blog/views/user.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from ..forms import PersonalInfoUpdateForm
+from ..tasks import send_mail
 
 User = get_user_model()
+
 
 class ProfileView(View):
 
@@ -26,6 +28,9 @@ class SettingsView(LoginRequiredMixin, View):
         })
 
     def post(self, request):
+        old_email = request.user.email
+        old_username = request.user.username
+
         form = PersonalInfoUpdateForm(
             data=request.POST,
             files=request.FILES,
@@ -33,6 +38,32 @@ class SettingsView(LoginRequiredMixin, View):
         )
 
         if form.is_valid():
+            context = {
+                "full_name": form.cleaned_data["full_name"],
+            }
+
+            new_username = form.cleaned_data["username"]
+            new_email = form.cleaned_data["email"]
+
+            email_changed = old_email != new_email
+            username_changed = old_username != new_username
+
+            if email_changed:
+                context["new_email"] = new_email
+
+            if username_changed:
+                context["old_username"] = old_username
+                context["new_username"] = new_username
+
+            if email_changed or username_changed:
+                send_mail.delay(
+                    to=[old_email],
+                    subject="Account Details Updated",
+                    template_name="email/account-update.txt",
+                    html_template_name="email/account-update.html",
+                    context=context,
+                )
+
             form.save()
             request.user.refresh_from_db()
 
