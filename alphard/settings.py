7c54cb2cc3ddc12fb691083a706b33356f263e50
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-01-30 09:46:04 -0600

    Installed psycopg postgresql database driver

--- alphard/settings.py
+++ alphard/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'alphard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alphard',
+        'USER': 'alphard',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
