a09116f0f37bbc349942928b23dd28724a79640a
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-01-30 11:58:02 -0600

    Cleanup

--- alphard/redis.py
+++
@@ -1,4 +0,0 @@
-from redis import Redis
-from django.conf import settings
-
-redis = Redis.from_url(settings.REDIS_URL, decode_responses=True)
--- alphard/urls.py
+++
@@ -1,8 +0,0 @@
-"""URL configuration for alphard project."""
-
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
