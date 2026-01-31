63e567848ae50598a401fc7170b2dde5da752aae
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-01-31 16:38:39 -0600

    Added sign in page and navigation between sign in and get started pages

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
+    SignInView,
     GetStartedView,
 )
 
 urlpatterns = [
+    path('sign-in', SignInView.as_view(), name='sign_in'),
     path('get-started', GetStartedView.as_view(), name='get_started'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,31 @@
 from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserCreationForm
+
+
+class SignInView(View):
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('home')
+
+        return render(request, 'sign-in.html', {
+            'form': AuthenticationForm(request),
+        })
+
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+
+        return render(request, 'sign-in.html', {
+            'form': form,
+        })
 
 
 class GetStartedView(View):
