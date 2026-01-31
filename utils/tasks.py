ae57099031f5a15d816854fe0bf611f03b7eb47b
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-01-31 11:59:02 -0600

    Added new celery task to send emails

---
+++ utils/tasks.py
@@ -0,0 +1,18 @@
+from celery import shared_task
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+@shared_task
+def send_mail(to, subject, template_name, html_template_name, context):
+    text_body = render_to_string(template_name, context)
+    html_body = render_to_string(html_template_name, context)
+
+    msg = EmailMultiAlternatives(
+        subject=subject,
+        body=text_body,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=to,
+    )
+    msg.attach_alternative(html_body, "text/html")
+    msg.send()
