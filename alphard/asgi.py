02307f2d3d905b9dbe27acfebc95150ceca09832
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-05-02 08:47:11 -0500

    Minor cleanup

--- alphard/asgi.py
+++ alphard/asgi.py
@@ -1,16 +1,6 @@
-"""
-ASGI config for alphard project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
+"""ASGI config to expose the ASGI callable as a module-level variable named ``application``."""
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alphard.settings')
-
 application = get_asgi_application()
--- alphard/wsgi.py
+++ alphard/wsgi.py
@@ -1,16 +1,6 @@
-"""
-WSGI config for alphard project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
+"""WSGI config to expose the WSGI callable as a module-level variable named ``application``."""
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alphard.settings')
-
 application = get_wsgi_application()
