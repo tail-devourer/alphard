290e65d4d2d068fb7ed7a27ca55914f4461b50db
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-05-02 08:21:39 -0500

    Changed database from SQLite3 to PostgreSQL

--- alphard/settings.py
+++ alphard/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'alphard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alphard',
+        'USER': 'alphard',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
